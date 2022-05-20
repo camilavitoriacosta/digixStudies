0cfd274350cce465c6b146ccf931394b9c1e637c
camilacosta <camilavitoria.c.costa@gmail.com>
2022-05-20 15:17:39 -0400

    Inserindo component na pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Formulario from "./components/Formulario";
+import Lista from "./components/Lista";
 
 function App() {
   return (
-    <Formulario />
+    <div>
+      <Formulario />
+      <Lista/>
+    </div>
   );
 }
 
