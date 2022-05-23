b85a3efb3a2c92e6ad6db9aa274626d50cae3c96
camilacosta <camilavitoria.c.costa@gmail.com>
2022-05-23 15:58:49 -0400

    Adicionando componente Cronometro

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,14 @@
+import style from './Cronometro.module.scss'
+import Botao from '../Botao'
+import Relogio from './Relogio';
+export default function Cronometro() {
+    return (
+        <div className={style.cronometro}>
+            <p className='titulo'>Escolha um card e inicie o cronometro</p>
+            <div className={style.relogioWrapper}>
+                <Relogio />
+            </div>
+            <Botao> Iniciar</Botao>
+        </div>
+    );
+}
