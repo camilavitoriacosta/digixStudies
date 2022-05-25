1529cbce456f348c81e3eb334676a7cbdde6f628
camilacosta <camilavitoria.c.costa@gmail.com>
2022-05-25 15:57:52 -0400

    criando conversor de tempo

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,7 +1,11 @@
 import style from './Cronometro.module.scss'
 import Botao from '../Botao'
 import Relogio from './Relogio';
+import { tempoParaSegundos } from '../../common/utils/date';
+
 export default function Cronometro() {
+    console.log('conversao', tempoParaSegundos('01:00:00'));
+
     return (
         <div className={style.cronometro}>
             <p className='titulo'>Escolha um card e inicie o cronometro</p>
